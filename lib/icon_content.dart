44d26a2cd783b4ad507b07624cc23a93c993cf8e
Haldiant <nhalwidinato@gmail.com>
2024-10-10 19:10:10 +0700

    update dart

--- lib/icon_content.dart
+++ lib/icon_content.dart
@@ -24,7 +24,7 @@ class IconContent extends StatelessWidget {
           icon,
           size: 80.0,
         ),
-        SizedBox(
+        const SizedBox(
           height: 15.0,
         ),
         Text(
